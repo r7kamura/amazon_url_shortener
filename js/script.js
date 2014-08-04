0a8409d564f9ca6015965a88b33473c23339ccad
negipo <negipo@gmail.com>
2014-08-04 22:01:07 +0900

    Add selectors to support some productions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var asinElement = document.getElementById("ASIN");
+  var asinElement = document.querySelector('#ASIN, input[name="idx.asin"], input[name="ASIN.0"]');
   if (asinElement) {
     chrome.extension.sendMessage(
       {},
