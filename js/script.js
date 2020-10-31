6eb86edb30e78c1bbc6de9a71ca5f15e8c65eeb4
r7kamura <r7kamura@gmail.com>
2020-10-31 17:49:52 +0900

    Run eslint js --fix

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.onload = () => {
-  const asinElement = document.querySelector('#ASIN, input[name="idx.asin"], input[name="ASIN.0"]');
+  const asinElement = document.querySelector('#ASIN, input[name="idx.asin"], input[name="ASIN.0"]')
   if (asinElement) {
     window.history.pushState(
       {},
-      "",
-      `/dp/${asinElement.value}`,
-    );
+      '',
+      `/dp/${asinElement.value}`
+    )
   }
-};
+}
