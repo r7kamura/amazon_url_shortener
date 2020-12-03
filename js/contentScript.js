b1547edd09b76e1746c496b21b29b2bfd8fd4a85
moroya <shoki0311@gmail.com>
2020-12-03 18:43:12 +0900

    Change pushState to replaceState

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const asinElement = document.querySelector('#ASIN, input[name="idx.asin"], input[name="ASIN.0"]')
   if (asinElement) {
-    window.history.pushState(
+    window.history.replaceState(
       {},
       '',
       `/dp/${asinElement.value}`
