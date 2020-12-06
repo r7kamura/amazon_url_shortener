392a3495878885b923efb9277af70ea42eaee7f4
r7kamura <r7kamura@gmail.com>
2020-12-06 19:06:35 +0900

    Add Amazon Prime Video page support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+### Added
+
+- Add Amazon Prime Video page support.
+
 ### Fixed
 
 - Fix duplicated www.amazon.com.br domain.
--- js/contentScript.js
+++ js/contentScript.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  const asinElement = document.querySelector('#ASIN, input[name="idx.asin"], input[name="ASIN.0"]')
+  const asinElement = document.querySelector('#ASIN, input[name="idx.asin"], input[name="ASIN.0"], input[name="titleID"]')
   if (asinElement) {
     window.history.pushState(
       {},
