a1dd7cd72a6b8b20a121d25211ab8e23bc4a626c
xyz <mugen.xyz@gmail.com>
2024-12-14 10:00:19 +0900

    fix: CSP error in Chrome 130+

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import { manifest } from "./manifest";
 
 export default defineConfig({
   plugins: [crx({ manifest })],
